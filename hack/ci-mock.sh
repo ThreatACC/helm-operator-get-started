754c0e0d3749bb2d5a317b6a1867010536a06bd6
Amir <amenly@gmail.com>
2021-08-22 14:09:57 +0300

    added echo up

--- hack/ci-mock.sh
+++ hack/ci-mock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-repository="stefanprodan/podinfo"
+repository="gcr.io/fine-balm-322411/echoup"
 branch="master"
 version=""
 commit=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1 | awk '{print tolower($0)}')
@@ -29,6 +29,6 @@ fi
 
 echo ">>>> Building image ${image} <<<<"
 
-docker build --build-arg GITCOMMIT=${commit} --build-arg VERSION=${version} -t ${image} -f Dockerfile.ci .
+docker build --build-arg GITCOMMIT=${commit} --build-arg VERSION=${version} -t ${image} -f ./echo/Dockerfile echo
 
 docker push ${image}
